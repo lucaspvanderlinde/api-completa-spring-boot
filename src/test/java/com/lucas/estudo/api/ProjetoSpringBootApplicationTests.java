be5c9809f9b7a8efe8ff15a06f3955026a66f480
Lucas Passos Vanderlinde <lucaspvanderlinde@hotmail.com>
2020-11-02 11:23:21 -0300

    Commit inicial dos arquivos do projeto.

---
+++ src/test/java/com/lucas/estudo/api/ProjetoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.estudo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
