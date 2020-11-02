fde734cf91c15bd62bf1d736b105d4998eb3ebf7
Lucas Passos Vanderlinde <lucaspvanderlinde@hotmail.com>
2020-11-02 17:18:08 -0300

    Ajustado import da classe principal

--- src/test/java/com/lucas/estudo/api/ProjetoSpringBootApplicationTests.java
+++ src/test/java/com/lucas/estudo/api/ProjetoSpringBootApplicationTests.java
@@ -1,6 +1,7 @@
 package com.lucas.estudo.api;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -9,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
-class ProjetoSpringBootApplicationTests {
+public class ProjetoSpringBootApplicationTests {
 	
 	@Test
 	public void contextLoads() {
