e519fad2cb2051ebadce7df158504b6b50d319d7
Lucas Passos Vanderlinde <lucaspvanderlinde@hotmail.com>
2020-11-04 09:42:10 -0300

    Criado Interface EmpresaService como boa pratica e a implementação desse serviço em uma outra classe  EmpresaServiceImpl.

---
+++ src/main/java/com/lucas/estudo/api/services/impl/EmpresaServiceImpl.java
@@ -0,0 +1,35 @@
+package com.lucas.estudo.api.services.impl;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucas.estudo.api.entities.Empresa;
+import com.lucas.estudo.api.repositories.EmpresaRepository;
+import com.lucas.estudo.api.services.EmpresaService;
+
+@Service
+public class EmpresaServiceImpl implements EmpresaService {
+
+	private static final Logger log = LoggerFactory.getLogger(EmpresaServiceImpl.class);
+
+	@Autowired
+	private EmpresaRepository empresaRepository;
+
+	@Override
+	public Optional<Empresa> buscarPorCnpj(String cnpj) {
+		log.info("Buscando uma empresa para o CNPJ: {}", cnpj);
+		// Retorno do tipo Optional.ofNullable, como pode ser nullo isso evita dar NullPointerException.
+		return Optional.ofNullable(empresaRepository.findByCnpj(cnpj));
+	}
+
+	@Override
+	public Empresa persistir(Empresa empresa) {
+		log.info("Persistindo empresa: {}", empresa);
+		return this.empresaRepository.save(empresa);
+	}
+
+}
